cce64267ee3089aeb190c7f0711eed9f3e1e31fc
Hayden Kowalchuk <819028+mrneo240@users.noreply.github.com>
2021-05-02 20:51:30 -0400

    fix: login redirect

--- registration/login.php
+++ registration/login.php
@@ -11,7 +11,7 @@ session_start();
 
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: home.html");
+    header("location: ../home.html");
     exit;
 }
 
